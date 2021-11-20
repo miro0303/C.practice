a82245bc9361dcca903997a1e8ac41c930c6f286
miro0303 <81725535+miro0303@users.noreply.github.com>
2021-11-20 14:48:31 +0900

    Update C test.cpp

--- C test.cpp
+++ C test.cpp
@@ -1,16 +1,54 @@
+#include <string.h>
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-	int i = 0;
+	char colorInput[] = "";
+	char color[] = {0};
+	int index = 0;
+	int red_num = 0;
+	int black_num = 0;
+	int host_money = 0;
+	printf("enter your color and money. If you are all done, press anything except red, and black.\n");
 	while(1)
 	{
-		printf("%d\n", i);
-		i += 1;
-		for(int o = 0; o < 10000000; o ++)
+		printf(": ");
+		scanf("%s", &colorInput);
+		if(strcmp(colorInput, "red") == 0)
 		{
-			printf("");
+			color[index] = 0;
+			index += 1;
+		}
+		else if(strcmp(colorInput, "black") == 0)
+		{
+			color[index] = 1;
+			index += 1;
+		}
+		else
+		{
+			break;
 		}
 	}
+	for(int i = 0; i < sizeof(color)/sizeof(color[1]); i ++)
+	{
+		if(color[i] == 1)
+		{
+			black_num += 1;
+		}
+		else if(color[i] == 0)
+		{
+			red_num += 1;
+		}
+	}
+	if(red_num > black_num)
+	{
+		printf("Answer is black!\n");
+		printf("host money is %d", red_num - black_num);
+	}
+	else
+	{
+		printf("Answer is red!\n");
+		printf("host money is %d", black_num - red_num);
+	}
+	return 0;
 }
